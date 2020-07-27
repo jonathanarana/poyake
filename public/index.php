20936f663ba267b8917ab41a7d30a17119feb6d9
Jonathan Arana <jonathanarana@primermaster.net>
2020-07-27 15:51:30 -0500

    async

--- public/index.php
+++ public/index.php
@@ -1,54 +1,16 @@
 <?php
+// activamos el reporte de errores
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+// cargamos el parametro de la url
+$route=isset($_GET['route'])?$_GET['route']:'index';
+// conectamos a la base de datos
 require '../lib/internal/database/MySQL.php';
 require '../lib/internal/database/Store.php';
-
 MySQL::conect();
+require '../app/loader.php';
+// cargamos todas la rutas
 
-$lista=Store::get_all();
+
  ?>
-<!DOCTYPE html>
-<html lang="es" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Lista de compras</title>
-  </head>
-  <body>
-    <h1>Lista de compras</h1>
-    <div>
-      <form class="" method="post" id="form">
-        <label for="">Cantidad:</label>
-        <input type="number" name="quantity" value="1" required>
-        <label for="">Descripción:</label>
-        <input type="text" name="description" value="" required>
-        <label for="">Precio:</label>
-        <input type="number" name="price" value="1.0" required>
-        <input type="submit" value="Agregar">
-      </form>
-    </div>
-    <table>
-      <thead>
-        <tr>
-          <th>Cantidad</th>
-          <th>Descripción</th>
-          <th>Precio</th>
-          <th>¿Adquirido?</th>
-        </tr>
-      </thead>
-      <tbody id="lista">
-        <?php foreach ($lista as $item): ?>
-        <tr>
-          <td><?php echo $item['quantity'] ?></td>
-          <td><?php echo $item['description'] ?></td>
-          <td><?php echo $item['price'] ?></td>
-          <td><?php echo $item['active'] ?></td>
-        </tr>
-        <?php endforeach; ?>
-      </tbody>
-    </table>
-    <script src="/js/app.js" charset="utf-8"></script>
-  </body>
-</html>
--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
 console.log('ready');
+get_all();
+function get_all() {
+  fetch('/?route=get-all-lista').then(function (res) {
+    return res.json();
+  }).then(function (data) {
+    let lista = document.getElementById('lista');
+    lista.innerHTML='';
+    data.forEach(function (item) {
+      lista.innerHTML += '<tr><td>'+item.quantity+'</td><td>'+item.description+'</td><td>'+item.price+'</td><td>'+item.active+'</td></tr>';
+    });
+  })
+}
+
+let submit = document.getElementById('submit');
+submit.addEventListener('click',function (event) {
+  event.preventDefault();
+  let datos = new FormData();
+
+  let data = document.querySelectorAll('input.input');
+  data.forEach(function (item) {
+    	datos.append(item.name, item.value);
+  });
+  datos.append('active', false);
+
+  console.log(JSON.stringify(datos));
+  fetch('/?route=add-to-list', {
+    method: 'POST',
+    body: datos
+  }).then(response => response)
+  .then(data => {
+    console.log(data);
+    get_all();
+  })
+});
